73cab7c82f0aacb5bc428018c9c6492b91886c05
Joost van Breukelen <Joost@imac-van-joost.fritz.box>
2016-09-01 21:21:24 +0200

    Initial Commit

---
+++ JvbDynamicTypeTableView/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  JvbDynamicTypeTableView
+//
+//  Created by Joost van Breukelen on 01-09-16.
+//  Copyright © 2016 Joost van Breukelen. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
